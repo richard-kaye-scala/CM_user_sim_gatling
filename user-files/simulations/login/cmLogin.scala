edc305ba8e0c589fcb54ea6a32b6ef046febaa37
Richard Kaye <richardkaye@Richards-MacBook-Pro.local>
2015-08-03 10:43:35 -0400

    First version of login script which attempts to pull in the authentication tokens from the auth login response.

--- user-files/simulations/login/cmLogin.scala
+++ user-files/simulations/login/cmLogin.scala
@@ -21,22 +21,27 @@ class cmLogin extends Simulation {
 		"X-Requested-With" -> "XMLHttpRequest",
 		"apiToken" -> "5146E1B2A482A7A34112B32BB06187FD33CE5BE62BB7E267BE7823966B197CF4AC81E6C6")
 
+	val headers_2 = Map(
+		"X-Requested-With" -> "XMLHttpRequest",
+		"Content-Type" -> "application/json; charset=UTF-8",
+		"token" -> "${session_token}")
+
     val uri1 = "http://marabunta.scala.com:8080/ContentManager/api/rest"
 
 	val scn = scenario("cmLogin")
 		.exec(http("login_api")
 			.post("/ContentManager/api/rest/auth/login")
 			.headers(headers_0)
-			.body(RawFileBody("cmLogin_0000_request.txt"))
-			.resources(http("request_1")
-			.get(uri1 + "/dashboard")
-			.headers(headers_1),
-            http("player_health")
-			.get(uri1 + "/playerhealth?offset=0&limit=999999&search=&sort=&count=0&filters=%7B%22playerHealthState%22%3A%7B%22values%22%3A%5B%22UNCLEARED%22%5D%7D%7D&fields=id")
-			.headers(headers_1),
-            http("hostedNetworks")
-			.get(uri1 + "/hostednetworks?offset=0&limit=999999&search=&sort=&count=0&filters=%7B%7D&fields=id")
-			.headers(headers_1)))
+			.body(StringBody("""{ "username": "administrator", "password": "scala1234" }""")).asJSON
+			.check(jsonPath("$.token").saveAs("cookie_token"))
+			.check(jsonPath("$.apiToken").saveAs("session_token"))
+			.check(status.is(201))
+		.exec(addCookie(Cookie("token","${cookie_token}")))
+		.exec(http("playerhealth")
+			.get("/ContentManager/api/rest/playerhealth?offset=0&limit=999999&search=&sort=&count=0&filters=%7B%22playerHealthState%22%3A%7B%22values%22%3A%5B%22UNCLEARED%22%5D%7D%7D&fields=id")
+			.headers(headers_2)
+			)
+
 
 	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 }
